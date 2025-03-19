09a374fd7da5eb93f5d76cfed4f8917909ab4b8d
JUN2973 <wnsdud2973@gmail.com>
2025-03-19 11:28:52 +0900

    수요일 과제1 nextDouble(), next() 이해x

---
+++ src/main/java/asgmt7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class asgmt7 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        double num;
+        System.out.println("3.14");
+        num = s.nextDouble();
+        System.out.println("3.14" + num);
+
+        String str;
+        System.out.println("난생처음");
+        str = s.next();
+        System.out.println("난생처음" + str );
+    }
+}
