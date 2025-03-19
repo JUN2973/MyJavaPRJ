495f13966c2c68208f284ad544e8d81869f13858
JUN2973 <wnsdud2973@gmail.com>
2025-03-19 16:29:04 +0900

    수요일 과제2 Interger.parseInt() ----> 문자열을 정수 Double.parseDouble() ----> 문자열을 실수

--- src/main/java/asgmt7.java
+++ src/main/java/asgmt7.java
@@ -5,13 +5,15 @@ public class asgmt7 {
         Scanner s = new Scanner(System.in);
 
         double num;
-        System.out.println("3.14");
+        System.out.print("실수를 입력하세요 : ");
         num = s.nextDouble();
         System.out.println("사용자가 입력한 값" + num);
 
         String str;
-        System.out.println("난생처음");
+        System.out.print("문자열을 입력하세요 : ");
         str = s.next();
-        System.out.println("사용자가 입력한 값" + str );
+        System.out.println("사용자가 입력한 값" + str);
+
+        s.close();
     }
 }
