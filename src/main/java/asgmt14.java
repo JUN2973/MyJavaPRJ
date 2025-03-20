207d0fa5fee1a174c6faedac1b064a512b4d759a
JUN2973 <wnsdud2973@gmail.com>
2025-03-21 08:54:31 +0900

    금요일 과제2 산술 연산자의 우선순위 다하기 --> 빼기 만약, 더하기 앞에 빼기가 있어도 우선순위가 동일하기 때문에 계산해도 상관없다.

---
+++ src/main/java/asgmt14.java
@@ -0,0 +1,9 @@
+public class asgmt14 {
+    public static void main(String[] args) {
+        int a = 3, b =4,c=5;
+        System.out.println(a+b-c);
+        System.out.println(a-c+b);
+        System.out.println(-c+a+b);
+
+    }
+}
