88e081a366557dc496a598c8eab09befd58b0cb9
JUN2973 <wnsdud2973@gmail.com>
2025-03-21 14:13:53 +0900

    변수의 개념 result - 정수로 표시 res - 실수로 표시 res = (float) - (flaot)로 변환해서 실수가 나올걸 계산해서 표시

--- src/main/java/Code02_08.java
+++ src/main/java/Code02_08.java
@@ -1,7 +1,20 @@
 public class Code02_08 {
     public static void main(String[] args) {
-        String str1 = "난생처음";
-        String str2 = ""
+
+        int num1, num2, result;
+        num1 = 100;
+        num2 = 60;
+
+        float res, res2;
+
+        result = num1 / num2;  //정수로 표시//
+        res = num1 / num2;     //실수로 표시//
+        res2 = (float) num1 / num2; //실수가 나올걸 계산해서 표시//
+        //(float) 형변환처리 잘할것!!!//
+
+        System.out.println(num1 + " / " + num2 + " = " + result);
+        System.out.println(num1 + " / " + num2 + " = " + res);
+        System.out.println(num1 + " / " + num2 + " = " + res2);
 
     }
 }
