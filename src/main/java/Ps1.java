2495e77fccf97cfa94eb0c58de6af480d32a8344
JUN2973 <wnsdud2973@gmail.com>
2025-03-24 12:21:23 +0900

    산술연산자 예제2

---
+++ src/main/java/Ps1.java
@@ -0,0 +1,22 @@
+public class Ps1 {
+    public static void main(String[] args){
+        int n1, n2, res;
+        n1 = 8;
+        n2 = 3;
+
+        res = n1 + n2;
+        System.out.println(res);
+
+        res = n1 - n2;
+        System.out.println(res);
+
+        res = n1 * n2;
+        System.out.println(res);
+
+        res = n1 / n2;
+        System.out.println(res);
+
+        res = n1 % n2;
+        System.out.println(res);
+    }
+}
