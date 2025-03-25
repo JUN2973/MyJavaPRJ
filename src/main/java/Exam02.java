9ca77850149b4d3fc7a0699286ce87f63442ce55
JUN2973 <wnsdud2973@gmail.com>
2025-03-25 19:06:53 +0900

    쪽지시험 연습2

---
+++ src/main/java/Exam02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exam02 {
+    public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+
+            int a, b;
+            double result;
+
+            a = s.nextInt();
+            b = s.nextInt();
+
+            result = (double) a / b;
+            System.out.println(a + "/" + b + "=" + rsult);
+
+            s.close();
+    }
+}
