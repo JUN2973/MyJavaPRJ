013522a717ec8042412685b02eb48e70a477aa08
JUN2973 <wnsdud2973@gmail.com>
2025-03-26 13:03:50 +0900

    쪽지시험 연습3

---
+++ src/main/java/Exam03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exam03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+        double result;
+
+        System.out.println("정수 a를 입력하세요: ");
+        a = sc.nextInt();
+        System.out.println("정수 b를 입력하세요: ");
+        b = sc.nextInt();
+
+        result = (double) a / b;
+
+        System.out.println(a + " / " + b + " = " + result);
+
+        sc.close();
+    }
+}
