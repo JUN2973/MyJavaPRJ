a53b65a8e59084386c2d3874f39a0f7660bdc29f
JUN2973 <wnsdud2973@gmail.com>
2025-04-01 17:30:05 +0900

    실수형 예제2

---
+++ src/main/java/Ps9.java
@@ -0,0 +1,11 @@
+public class Ps9 {
+    public static void main(String[] args){
+        int n1 = 1;
+        int n2 = 2;
+
+        System.out.println(n1+n2);
+        System.out.println(n1-n2);
+        System.out.println(n1*n2);
+        System.out.println(n1/n2);
+    }
+}
