52bb6f39b700b82c3144590ebfc820e0d1b936cd
JUN2973 <wnsdud2973@gmail.com>
2025-04-01 17:57:01 +0900

    문자열 예제2

---
+++ src/main/java/Ps12.java
@@ -0,0 +1,8 @@
+public class Ps12 {
+    public static void main(String[] args) {
+        String var1 = "난생처음 \n" +
+                      "자바를 \n" +
+                      "열공 중입니다.";
+            System.out.println(var1);
+    }
+}
