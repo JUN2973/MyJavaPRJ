9f2e195dbe0b3c0b2a8e5d13ab79d9ea6badc77f
JUN2973 <wnsdud2973@gmail.com>
2025-04-02 15:43:12 +0900

    IF

---
+++ src/main/java/Ps18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ps18 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else{
+            System.out.println("==");
+            }
+        s.close();
+    }
+
+}
