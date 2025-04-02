1b26541f39b84b41dcbbe152f1c79eac0042f47f
JUN2973 <wnsdud2973@gmail.com>
2025-04-02 17:52:59 +0900

    Exam6

---
+++ src/main/java/Exam6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exam6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String result = (a + b).toLowerCase().replaceAll(" ", "");
+        System.out.println(result);
+
+        sc.close();
+    }
+
+}
