c23ec6f6bcd2ea07bc9249d4a3e0f99f86491dcb
JUN2973 <wnsdud2973@gmail.com>
2025-04-03 20:10:57 +0900

    정답 뉴비 올드비

---
+++ src/main/java/Ps23.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ps23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        if (m == 1 || m == 2) {
+            System.out.println("NEWBIE!");
+        } else if (m <= n) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TlE!");
+        }
+        sc.close();
+    }
+}
