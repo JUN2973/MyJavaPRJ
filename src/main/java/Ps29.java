69db4b19f4c1ea743baad662898e8a9ceb328ff8
JUN2973 <wnsdud2973@gmail.com>
2025-04-04 15:58:45 +0900

    if

---
+++ src/main/java/Ps29.java
@@ -0,0 +1,9 @@
+public class Ps29 {
+    public static void main(String[] args) {
+        int num = 90;
+
+        if (num < 100) {
+            System.out.println("100보다 작습니다.");
+        }
+    }
+}
