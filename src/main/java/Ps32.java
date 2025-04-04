6137a1dd204875d0f788bca91eb4fcf48827d4e4
JUN2973 <wnsdud2973@gmail.com>
2025-04-04 16:00:53 +0900

    if else scanner

---
+++ src/main/java/Ps32.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ps32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        System.out.println("숫자를 입력:");
+        num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("짝수입니다.");
+        } else {
+            System.out.println("홀수입니다.");
+        }
+
+
+        System.out.println("프로그램 끝!");
+
+
+        sc.close();
+    }
+}
