ad85504a734678f786ff81a30d0950cf2801345a
JUN2973 <wnsdud2973@gmail.com>
2025-04-08 20:07:55 +0900

    for

---
+++ src/main/java/Ps41.java
@@ -0,0 +1,9 @@
+public class Ps41 {
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 8 - 7; a++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
