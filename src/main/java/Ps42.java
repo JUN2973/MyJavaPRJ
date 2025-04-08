d99d753c2c5442756a3022a24e4182c08fa6f44a
JUN2973 <wnsdud2973@gmail.com>
2025-04-08 20:08:07 +0900

    for

---
+++ src/main/java/Ps42.java
@@ -0,0 +1,17 @@
+public class Ps42 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 7; i++) {
+
+            if (i <= 4) {
+                for (int a = 1; a <= i; a++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int a = 1; a <= 8-i; a++)
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+}
