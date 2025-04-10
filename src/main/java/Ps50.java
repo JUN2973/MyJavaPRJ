f8c5ea06642c70b010157e2be2ec287174105686
JUN2973 <wnsdud2973@gmail.com>
2025-04-10 19:22:12 +0900

    while

---
+++ src/main/java/Ps50.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ps50 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("숫자를 입력하세요 (0 입력 시 종료) : ");
+
+            int a = sc.nextInt();
+
+            if (a == 0){
+                System.out.println("0을 입력해서 while문 종료");
+                break;
+            }
+
+            System.out.println(a + "를 입력하셨습니다. while문 계속");
+        }
+    }
+}
