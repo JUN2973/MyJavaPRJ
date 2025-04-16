4ce591de9dad77abbbe09877af35b98daa44adf4
JUN2973 <wnsdud2973@gmail.com>
2025-04-16 18:02:50 +0900

    rabbit

---
+++ src/main/java/Ps61.java
@@ -0,0 +1,22 @@
+class Rabbit1 {
+    String shape;
+    int xPos;
+    int yPos;
+
+    Rabbit1 () {
+        shape = "토끼";
+    }
+
+    void setPosition (int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Ps61{
+    public static void main(String[] args) {
+        Rabbit1 rabbit1 = new Rabbit1();
+
+        System.out.println("rabbit 1의 모양 : " + rabbit1.shape);
+    }
+}
