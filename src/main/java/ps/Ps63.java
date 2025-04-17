acb15873e781fece89f61b1262d87267bd0e4f31
JUN2973 <wnsdud2973@gmail.com>
2025-04-17 16:22:24 +0900

    Rabbit

---
+++ src/main/java/ps/Ps63.java
@@ -0,0 +1,17 @@
+package ps;
+
+class Rabbit3 {
+        public String shape;
+        public int xPos;
+        public int yPos;
+
+}
+
+public class Ps63 {
+    public static void main(String[] args) {
+        Rabbit3 rabbit = new Rabbit3();
+
+        rabbit.xPos = 100;
+        rabbit.yPos = 200;
+    }
+}
