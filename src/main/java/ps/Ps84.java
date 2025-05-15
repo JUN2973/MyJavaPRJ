4e548ab19f4c2201051dc383175f26c6fc5a0a3a
JUN2973 <wnsdud2973@gmail.com>
2025-05-15 16:23:43 +0900

    list 컬렉션

---
+++ src/main/java/ps/Ps84.java
@@ -0,0 +1,23 @@
+package ps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ps84 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>(); //유지보수가 편하다
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+
+        int listSize = list.size();
+
+        System.out.println("4세대 반복문 - 병렬구조");
+
+        list.parallelStream().forEach(name -> System.out.println("name : " + name));
+    }
+}
